2e60ccf83b62254e296bfbe57a610297aa90aec4
yjleev <sjlee081010@gmail.com>
2026-04-09 09:26:07 +0900

    feat: cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
